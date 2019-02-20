479629120db7a144fd8a8989f96c11f251cd6f05
fakiejohnny <boettger.conrad@gmail.com>
2019-02-20 12:31:48 +0100

    so

--- src/components/panel/index.js
+++ src/components/panel/index.js
@@ -2,25 +2,10 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Panel extends Component {
-	state = {
-		count: 0,
-		winns: 0
-	};
 
-	increment = () => {
-		this.setState({ count: this.state.count+1 });
-		
-		if (this.state.count > 11) {
-			this.setState({
-				winns: this.state.winns+1,
-				count: 1
-			});
-		}
-	};
-
-	render({ username }, { count, winns }) {
+	render({ username, increment, count, winns }, {}) {
 		return (
-			<div class={style.panel} onClick={this.increment}>
+			<div class={style.panel} onClick={increment}>
 				<p class={style.username}>{username}</p>
 				<p class={style.count}>{count}</p>
 				<p class={style.winns}>{winns}</p>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,11 +5,49 @@ import Panel from './../../components/panel';
 
 export default class Home extends Component {
 
+	state = {
+		count1 : 0,
+		count2 : 0,
+		winns1 : 0,
+		winns2 : 0
+	};
+
+	increment1 = () => {
+		this.setState({ count1: this.state.count1 + 1 });
+
+		if (this.state.count1 >= 11 && this.state.count1 - this.state.count2 >= 2) {
+			console.log("Player 1 win!");
+			this.setState({
+				winns1: this.state.winns1 + 1
+			});
+			this.reset();
+		}
+	};
+
+	increment2 = () => {
+		this.setState({ count2: this.state.count2 + 1 });
+
+		if (this.state.count2 >= 11 && this.state.count2 - this.state.count1 >= 2) {
+			console.log("Player 2 win!");
+			this.setState({
+				winns2: this.state.winns2 + 1
+			});
+			this.reset();
+		}
+	};
+
+	reset = () => {
+		this.setState({
+			count1 : 0,
+			count2 : 0,
+		});
+	}
+
 	render() {
 		return (
             <div class={style.home}>
-                <Panel username="Conrad"/>
-                <Panel username="Paul"/>
+                <Panel username="Conrad" count={this.state.count1} winns={this.state.winns1} increment={this.increment1}/>
+                <Panel username="Paul" count={this.state.count2} winns={this.state.winns2} increment={this.increment2}/>
             </div>
 		);
 	}
